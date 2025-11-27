a0fb1b4db1594c50d281beea731cc67c254deecb
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2025-11-28 00:34:13 +0300

    Update Coach.java

--- src/ru/yandex/practicum/gym/Coach.java
+++ src/ru/yandex/practicum/gym/Coach.java
@@ -1,17 +1,29 @@
+package ru.yandex.practicum.gym;
+
 import java.util.Objects;
 
 public class Coach {
-    //фамилия
     private String surname;
-    //имя
     private String name;
-    //отчество
     private String middleName;
 
     public Coach(String surname, String name, String middleName) {
         this.surname = surname;
         this.name = name;
         this.middleName = middleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coach coach = (Coach) o;
+        return Objects.equals(surname, coach.surname) && Objects.equals(name, coach.name) && Objects.equals(middleName, coach.middleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(surname, name, middleName);
     }
 
     public String getSurname() {
@@ -24,20 +36,5 @@ public class Coach {
 
     public String getMiddleName() {
         return middleName;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Coach coach = (Coach) o;
-        return Objects.equals(surname, coach.surname) &&
-                Objects.equals(name, coach.name) &&
-                Objects.equals(middleName, coach.middleName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(surname, name, middleName);
     }
 }
